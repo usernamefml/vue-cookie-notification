34fcbf2e523531dbfde47494c67a53731d8c5d2b
davidwroche <davidwroche@yahoo.co.uk>
2018-02-18 16:41:10 +0000

    first commit

---
+++ cookie.vue
@@ -0,0 +1,55 @@
+<template>
+<div>
+<div id ="vue_cookie_privacy" v-if="cookie != true" v-cloak>
+     <span>
+        In order to give you the best experience, our website uses cookies. By continuing to use this site, you agree to our use of cookies.
+        <button class="cookiebutton" v-on:click="accept_cookie">Accept</button>
+     </span>
+   </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'VueCookie',
+    data: function() {
+        return {
+            data: "star rating"
+        }
+    }
+}
+</script>
+
+<style scoped>
+[v-cloak] {
+  display: none;
+}
+
+
+#vue_cookie_privacy {
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  background-color: #e8e8e8;
+  color: #333333;
+  font-size: 14px;
+  text-align: center;
+  font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;
+  line-height: 1.42857143;
+
+}
+
+.cookiebutton {
+  background-color: #4CAF50;
+  border: none;
+  color: white;
+  padding: 10px 25px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  border-radius: 5px 5px 5px 5px;
+  cursor: pointer;
+}
+</style>
